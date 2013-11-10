cb3de0732ba2a55ad8e1e1cd769e0a5a07b34660
soulgalore <peter@soulgalore.com>
2013-11-11 00:32:56 +0100

    first commit

---
+++ sitespeed.io.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class SitespeedIo < Formula
+  depends_on 'phantomjs'
+  depends_on 'browsertime'
+  homepage 'http://www.sitespeed.io'
+  url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.2-beta-1/sitespeed.io-2.2-BETA-1.tar.gz'
+  sha1 '1cb717de04e4929be371bea64f97524c661af142'
+
+  def install
+    prefix.install_metafiles
+    bin.install('bin/sitespeed.io')
+    bin.install('bin/sitespeed-sites.io')
+    prefix.install Dir['*']
+  end
+
+  test do
+    system "sitespeed.io", "-V"
+  end
+end
