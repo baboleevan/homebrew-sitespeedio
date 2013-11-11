6e28b0aa56ab56ac5f2d9ef29addc0af82fc1157
soulgalore <peter@soulgalore.com>
2013-11-11 10:33:50 +0100

    added version

--- sitespeed.io.rb
+++ sitespeed.io.rb
@@ -4,9 +4,10 @@ class SitespeedIo < Formula
   depends_on 'phantomjs'
   depends_on 'browsertime'
   homepage 'http://www.sitespeed.io'
+  version '2.2-beta-1'  
   url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.2-beta-1/sitespeed.io-2.2-BETA-1.tar.gz'
   sha1 '1cb717de04e4929be371bea64f97524c661af142'
-
+  
   def install
     prefix.install_metafiles
     bin.install('bin/sitespeed.io')
