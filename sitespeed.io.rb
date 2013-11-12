224ef4c20e104fee3eac3fe85352c006c6397fd2
soulgalore <peter@soulgalore.com>
2013-11-12 11:55:24 +0100

    new version

--- sitespeed.io.rb
+++ sitespeed.io.rb
@@ -4,7 +4,7 @@ class SitespeedIo < Formula
   depends_on 'phantomjs'
   homepage 'http://www.sitespeed.io'
   version '2.2-beta-1'  
-  url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.1.2/sitespeed.io-2.2.tar.gz'
+  url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.2/sitespeed.io-2.2.tar.gz'
   sha1 '476a37544243622e484c800c7cc5cf74212aaea0'
   
   def install
