cf7fdfcca5954648d306ab31a4af66d92a3eebcd
Tobias Lidskog <tobias@lidskog.se>
2013-12-03 00:17:38 +0100

    Update to 2.2.3

--- sitespeed.io.rb
+++ sitespeed.io.rb
@@ -3,8 +3,8 @@ require 'formula'
 class SitespeedIo < Formula
   depends_on 'phantomjs'
   homepage 'http://www.sitespeed.io'
-  url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.2.2/sitespeed.io-2.2.2.tar.gz'
-  sha1 '0029e1d2702445580f001172e0899d2afb80afa7'
+  url 'https://github.com/sitespeedio/sitespeed.io/releases/download/v2.2.3/sitespeed.io-2.2.3.tar.gz'
+  sha1 'b25cdbba536331648ab6d937db259faf57ae035b'
 
   def install
     prefix.install_metafiles
